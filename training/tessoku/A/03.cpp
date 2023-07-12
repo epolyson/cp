a7e608dedf0859eb6ccf0eeaf9084af43d345227
epolyson <eikipolyson@gmail.com>
2023-07-12 22:18:48 +0900

    20230712

---
+++ training/tessoku/A/03.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP3(i, s, n) for(int i = (s); i < (int)(n); i++)
+#define REP_R(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define REP3_R(i, s, n) for(int i = (int)(n) - 1; i >= (int)(s); i--)
+#define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e18
+#define PI 3.141592653589
+
+#define VI vector<int>
+#define VL vector<ll>
+#define VVI vector<vector<int> >
+#define VVL vector<vector<ll> >
+
+template <class T>
+inline bool chmin(T &a, T b) {
+   if(a > b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+   if(a < b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline void printVec(vector<T> v) {
+   REP(i,v.size()){
+      if(i) cout << " ";
+      cout << v[i];
+      } cout << endl;
+}
+
+int main(){
+    FAST
+    int N, K;
+    cin >> N >> K;
+    VI P(N);
+    REP(i, N) cin >> P[i];
+    VI Q(N);
+    REP(i, N) cin >> Q[i];
+
+    bool flg = false;
+    REP(i,N) REP(j,N){
+        if(P[i] + Q[j] == K) flg = true;
+    }
+    cout << (flg ? "Yes" : "No") << endl;
+    return 0;
+}
---
+++ training/tessoku/B/02.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP3(i, s, n) for(int i = (s); i < (int)(n); i++)
+#define REP_R(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define REP3_R(i, s, n) for(int i = (int)(n) - 1; i >= (int)(s); i--)
+#define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e18
+#define PI 3.141592653589
+
+#define VI vector<int>
+#define VL vector<ll>
+#define VVI vector<vector<int> >
+#define VVL vector<vector<ll> >
+
+template <class T>
+inline bool chmin(T &a, T b) {
+   if(a > b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+   if(a < b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline void printVec(vector<T> v) {
+   REP(i,v.size()){
+      if(i) cout << " ";
+      cout << v[i];
+      } cout << endl;
+}
+
+int main(){
+    FAST
+    // 100の約数
+    // set<int> st;
+    // REP(i,101) {
+    //     if(100%i == 0) st.insert(i);
+    // }
+
+    bool flg = false;
+    int A, B;
+    cin >> A >> B;
+    REP3(i,A,B+1){
+        if(100%i == 0) flg = true;
+    }
+    if(flg) cout << "Yes\n";
+    else cout << "No\n";
+    return 0;
+
+}

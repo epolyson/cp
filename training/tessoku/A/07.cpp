95097497a0611751e5d7c7c38d80836f126923f7
epolyson <eikipolyson@gmail.com>
2023-07-18 22:17:17 +0900

    20230718

---
+++ training/tessoku/A/07.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP3(i, s, n) for(int i = (s); i < (int)(n); i++)
+#define REP_R(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define REP3_R(i, s, n) for(int i = (int)(n) - 1; i >= (int)(s); i--)
+#define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e18
+#define PI 3.141592653589
+
+#define VI vector<int>
+#define VL vector<ll>
+#define VVI vector<vector<int> >
+#define VVL vector<vector<ll> >
+
+template <class T>
+inline bool chmin(T &a, T b) {
+   if(a > b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+   if(a < b){ a = b; return true;}
+   return false;
+}
+template <class T>
+inline void printVec(vector<T> v) {
+   REP(i,v.size()){
+      if(i) cout << " ";
+      cout << v[i];
+      } cout << endl;
+}
+
+int main(){
+    FAST
+    int D, N;
+    cin >> D >> N;
+    VI gap(100009,0);
+    int L, R;
+    REP3(i,1,N+1) { 
+        cin >> L >> R;
+        gap[L]++;
+        gap[R+1]--;
+    }
+
+    VI participant(100009,0);
+    REP3(i,1,D+1){
+        participant[i] = gap[i] + participant[i-1];
+    }
+    REP3(i,1,D+1) cout << participant[i] << endl;
+    return 0;
+}
